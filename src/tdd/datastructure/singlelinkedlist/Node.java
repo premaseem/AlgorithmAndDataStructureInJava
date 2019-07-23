c75c485911bbaea463246bfcd3c83553ee6ce626
Aseem Jain <premaseem.webmaster@gmail.com>
2019-07-22 21:16:52 -0500

    single linked list

---
+++ src/tdd/datastructure/singlelinkedlist/Node.java
@@ -0,0 +1,10 @@
+package tdd.datastructure.singlelinkedlist;
+
+public class Node {
+
+    public Node(int data){
+        this.data = data;
+    }
+    public int data;
+    public Node next = null;
+}
