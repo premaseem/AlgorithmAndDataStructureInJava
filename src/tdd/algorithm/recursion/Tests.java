2a84899fca128882ffe877c469cf453c5cd82cda
Aseem Jain <premaseem.webmaster@gmail.com>
2019-07-23 17:31:13 -0500

    Recursion example count down, factorial and fibonacci

---
+++ src/tdd/algorithm/recursion/Tests.java
@@ -0,0 +1,36 @@
+package tdd.algorithm.recursion;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Tests {
+
+    RecursiveAlgo rec = new RecursiveAlgo();
+
+    @Test
+    public void testDownCounter(){
+        rec.downCounter(10);
+    }
+
+    @Test
+    public void testFactorial(){
+       assertEquals(120,rec.factorialWithRecursion(5));
+       assertEquals(120,rec.factorialWithLoop(5));
+    }
+
+    @Test
+    public void testFibonacci(){
+        assertEquals(5,rec.fibonacciWithLoop(5));
+        assertEquals(8,rec.fibonacciWithLoop(6));
+        assertEquals(13,rec.fibonacciWithLoop(7));
+        assertEquals(21,rec.fibonacciWithLoop(8));
+    }
+
+    @Test
+    public void testFibonacci1(){
+        assertEquals(5,rec.fibonacciRecursion(5));
+
+    }
+
+}
