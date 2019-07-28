4dd1abae363c8f21a6b7b41104e4d06956c76fd7
Aseem Jain <premaseem.webmaster@gmail.com>
2019-07-28 16:14:35 -0500

    Heap Data Structure

---
+++ src/tdd/datastructure/heap/Node.java
@@ -0,0 +1,8 @@
+package tdd.datastructure.heap;
+
+public class Node {
+    int key;
+    public Node(int key){
+        this.key = key;
+    }
+}
---
+++ src/tdd/datastructure/heap/Test.java
@@ -0,0 +1,32 @@
+package tdd.datastructure.heap;
+
+public class Test {
+    Heap heap = new Heap(20);
+
+    @org.junit.Test
+    public void insertTest(){
+        heap.insert(10);
+        heap.insert(100);
+        heap.insert(20);
+//        heap.print();
+
+        heap.insert(50);
+        heap.insert(15);
+        heap.insert(35);
+        heap.insert(45);
+        heap.insert(5);
+
+        heap.insert(51);
+        heap.insert(11);
+        heap.insert(351);
+        heap.insert(451);
+        heap.insert(51);
+        heap.insert(9);
+        heap.insert(10);
+//        heap.print();
+        heap.printTree();
+//        heap.displayHeap1();
+
+    }
+
+}
