728d84b74a0fa8a16a0d96aa1df2be55c14bee5d
Aseem Jain <premaseem.webmaster@gmail.com>
2019-07-28 13:23:40 -0500

    Binary Search Tree algo remove code

---
+++ src/tdd/datastructure/binaryTree/Node.java
@@ -0,0 +1,13 @@
+package tdd.datastructure.binaryTree;
+
+public class Node {
+
+    int key;
+    private String value;
+    public Node leftNode, rightNode;
+
+    public Node(int key){
+        this.key = key;
+    }
+
+}
