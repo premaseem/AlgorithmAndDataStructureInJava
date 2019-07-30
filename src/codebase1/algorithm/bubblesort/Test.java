47518aef56b45b53b78655b79738e67f279a2b6c
Aseem Jain <premaseem.webmaster@gmail.com>
2019-07-30 10:57:26 -0500

    Bubble sort algo implementation with Test Driven approach in Java

---
+++ src/codebase1/algorithm/bubblesort/Test.java
@@ -0,0 +1,19 @@
+package codebase1.algorithm.bubblesort;
+
+public class Test {
+
+    @org.junit.Test
+    public void test(){
+        BubbleSort bs = new BubbleSort();
+        int arr[] = {40, 20, 50, 100, 40, 80};
+        //int arr[] = {20, 40, 40, 50, 50, 100};
+        bs.sort(arr);
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.println(arr[i]);
+            if (arr[i] > arr[i + 1]) {
+                assert false;
+            }
+        }
+    }
+}
