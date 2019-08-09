9ef23cf2c2c24c77a54c39598cf652b18a154f2c
Aseem Jain <premaseem.webmaster@gmail.com>
2019-08-08 23:09:46 -0500

    Apple interview question find 1st non-recurring character in string

---
+++ src/codechallenge/easy/NonRecurringCharacter.java
@@ -0,0 +1,79 @@
+package codechallenge.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/*
+//String = abchhyiayjj , find 1st non-recurring character in string
+ */
+public class NonRecurringCharacter {
+
+    @Test
+    public void test(){
+        // Brute Force version 1 tests
+        Assert.assertEquals( 'b' , method("abchhyiayjj"));
+        Assert.assertEquals( 's' , method("aseema"));
+        Assert.assertEquals( 'n' , method("sonysoty"));
+        Assert.assertEquals( 'd' , method("zzd"));
+        Assert.assertEquals( '0' , method("aaee"));
+
+        // Optimized version 2 tests
+        Assert.assertEquals( 'b' , method2("abchhyiayjj"));
+        Assert.assertEquals( 's' , method2("aseema"));
+        Assert.assertEquals( 'n' , method2("sonysoty"));
+        Assert.assertEquals( 'd' , method2("zzd"));
+        Assert.assertEquals( '0' , method2("aaee"));
+    }
+
+
+    // Brute Force version 1.0
+    public static char method(String s) {
+
+        char[] chars = s.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            char ca = chars[i];
+            if (charNonRepeates(ca, chars)){
+                return ca;
+            }
+        }
+        return '0';
+    }
+
+    private static boolean charNonRepeates(char c, char[] arr){
+        int charCount = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] == c){
+                charCount++;
+            }
+        }
+        return charCount  == 1;
+    }
+
+
+    // Optimized version 2.0
+     public char method2(String s){
+
+        Map<Character,Integer> cm = new TreeMap<>();
+
+        char[] chars = s.toCharArray();
+        for(char c: chars){
+            if(null == cm.get(c)){
+                cm.put(c,1);
+            }else{
+                Integer count = cm.get(c);
+                cm.put(c,++count);
+            }
+        }
+
+        for(char c: chars){
+            if (cm.get(c) == 1){
+                return c;
+            }
+        }
+      return '0';
+    }
+}
