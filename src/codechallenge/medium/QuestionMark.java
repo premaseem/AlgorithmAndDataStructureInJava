1bae263ba11d935b73113bd7612537656fc3c0a3
Aseem Jain <premaseem.webmaster@gmail.com>
2019-08-09 18:50:40 -0500

    Code challenge: Question mark count between 2 numbers how adds up to 10

--- src/codechallenge/medium/QuestionMark.java
+++ src/codechallenge/medium/QuestionMark.java
@@ -9,7 +9,8 @@ public class QuestionMark {
 
     @Test
     public void test(){
-        assert QuestionsMarks("acc?7??sss?3rr1??????5");
+        assert QuestionsMarks("ac2c?7??sss?3rr1??????5");
+        assert QuestionsMarks2("acc?7??sss?3rr1??????5");
     }
 
     public static boolean QuestionsMarks(String str) {
@@ -37,24 +38,52 @@ public class QuestionMark {
 
         String[] sa = p.split(" ");
         for(int i=0;i<sa.length;i=i+2){
-            String pc = str.substring(sa[i].indexOf(i),sa[i].indexOf(i+1));
+            String pc = str.substring(str.indexOf(sa[i].charAt(i)),str.indexOf(sa[i].charAt(i+1)));
             int ct = pc.length() - pc.replace("?","").length();
             if(ct==3){
                 return true;
             }
         }
-        // str.indexOf(ln.get())
 
+        return false;
+    }
+
+
+    public  boolean QuestionsMarks2(String str) {
+
+        StringBuilder buffer = new StringBuilder();
+
+        for(int i=0; i < str.length(); i++){
+            if(Character.isDigit(str.charAt(i)) || str.charAt(i)=='?'){
+                buffer.append(str.charAt(i));
+            }
+        }
+
+        for(int i=0; i < buffer.length(); i++){
+            if(Character.isDigit(buffer.charAt(i))){
+                if(i+4 <= buffer.length()){
+                    if(buffer.charAt(i+1)=='?'
+                            && buffer.charAt(i+2) == '?'
+                            && buffer.charAt(i+3) == '?'
+                            && Character.isDigit(buffer.charAt(i+4))){
+
+                        int num1 = Character.getNumericValue(buffer.charAt(i));
+                        int num2 = Character.getNumericValue(buffer.charAt(i+4));
+
+                        if(num1 + num2 == 10){
+                            return true;
+                        }
+                    }
+                    if(i+5 < buffer.length()){
+                        i = i+5;
+                    }
+                }
+            }
+        }
         return false;
 
     }
 
-
-    public static void main (String[] args) {
-        // keep this function call here
-//        Scanner s = new Scanner(System.in);
-//        System.out.print(QuestionsMarks(s.nextLine()));
-    }
 
 
 }
