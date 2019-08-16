e45590522d903b6f9498e548f61de87596da7fdd
Aseem Jain <premaseem.webmaster@gmail.com>
2019-08-16 18:41:48 -0500

    Stack using Array

---
+++ src/codebase3/datastructures/StackWithArray.java
@@ -0,0 +1,71 @@
+package codebase3.datastructures;
+
+
+// Test Driven Development by Aseem Jain
+
+import org.junit.Test;
+
+
+public class StackWithArray {
+
+    int[] sa;
+    int pointer;
+
+    public StackWithArray(){
+        sa = new int[10];
+        pointer=-1;
+    }
+
+
+    boolean push(int e){
+        if (pointer >= sa.length){
+            return false;
+        }
+        pointer++;
+        sa[pointer] = e;
+        return true;
+    }
+
+    boolean isEmpty(){
+        return pointer < 0;
+    }
+
+    int pop(){
+
+        int v = sa[pointer];
+        pointer--;
+        return v;
+    }
+
+    boolean search(int e){
+        for ( int item : sa){
+            if (item == e){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Test Driven Development by Aseem Jain
+    @Test
+    public void test() {
+
+        StackWithArray sa = new StackWithArray();
+        assert sa.isEmpty();
+        assert sa.push(7);
+        assert sa.push(4);
+
+        assert sa.search(7);
+        assert ! sa.search(6);
+
+        assert ! sa.isEmpty();
+        assert 4 == sa.pop();
+        assert 7 == sa.pop();
+        assert sa.isEmpty();
+
+
+
+
+    }
+}
+
