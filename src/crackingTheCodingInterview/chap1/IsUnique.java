b43011fe35809ea52ddfe37397162120daeca85b
Aseem Jain <premaseem.webmaster@gmail.com>
2019-08-31 01:10:01 -0500

    Is Unique: Implement an algorithm to determine if a string has all unique characters.

---
+++ src/crackingTheCodingInterview/chap1/IsUnique.java
@@ -0,0 +1,69 @@
+package crackingTheCodingInterview.chap1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+
+/**
+ * Is Unique: Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ */
+
+public class IsUnique {
+    // Test Driven Development by Aseem Jain
+    @Test
+    public void test() {
+
+       assert sol1("Ben");
+       assert !sol1("Benny");
+
+        assert sol2("Ben");
+        assert !sol2("Aseem");
+
+        assert sol3("Ben");
+        assert !sol3("Aseem");
+
+
+    }
+
+
+    private boolean sol1(String s) {
+        // find index and return if repeated
+        for (int i = 0; i < s.length()-1; i++) {
+            for (int j = i+1; j < s.length(); j++) {
+                if(s.charAt(i) == s.charAt(j)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean sol2(String s) {
+        // find index and return if repeated
+        for (int i = 0; i < s.length(); i++) {
+            if(s.substring(i+1).indexOf(s.charAt(i)) != -1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean sol3(String s) {
+        // find index and return if repeated
+        HashSet<Character> cs = new HashSet<>(s.length());
+        char[] chars = s.toCharArray();
+        for ( char c: chars){
+            if(cs.contains(c)){
+                return false;
+            }else{
+                cs.add(c);
+            }
+        }
+
+        return true;
+    }
+
+
+}
