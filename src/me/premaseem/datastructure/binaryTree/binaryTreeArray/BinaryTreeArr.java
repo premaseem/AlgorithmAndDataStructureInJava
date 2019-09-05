f9606e2d9f8273ee44f0799acb248bf2ee4e01cc
Aseem Jain <premaseem.webmaster@gmail.com>
2019-09-04 22:15:32 -0500

    Binary tree implementation using arry

---
+++ src/me/premaseem/datastructure/binaryTree/binaryTreeArray/BinaryTreeArr.java
@@ -0,0 +1,76 @@
+package me.premaseem.datastructure.binaryTree.binaryTreeArray;
+
+public class BinaryTreeArr {
+
+    int[] a;
+    int lui =0; // last used index
+
+    public BinaryTreeArr(int size){
+       a = new int[size +1];
+    }
+
+    public boolean insert(int v){
+        if (lui >= a.length-1){
+            System.out.println("cannot insert");
+            return false;
+        }
+        a[++lui] = v;
+        return true;
+    }
+
+    public int search(int v){
+        for (int i = 1; i < lui; i++) {
+            if(a[i] == v){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean delete(int v){
+        int s = search(v);
+        if(s != -1){
+            a[s] = a[lui--];
+            return true;
+        }
+            System.out.println("could not find element");
+        return false;
+    }
+
+    public void pre(int i){
+        if(i > lui){
+            return;
+        }
+//        System.out.print("Pre order traversal ");
+        System.out.print(" >> "+a[i]);
+        pre(i*2);
+        pre(i*2+1);
+    }
+
+    public void post(int i){
+        if(i > lui){
+            return;
+        }
+//        System.out.print("Post order traversal ");
+        pre(i*2);
+        pre(i*2+1);
+        System.out.print(" >> "+a[i]);
+    }
+
+    public void in(int i){
+        if(i > lui){
+            return;
+        }
+//        System.out.print("In order traversal ");
+        pre(i*2);
+        System.out.print(" >> "+a[i]);
+        pre(i*2+1);
+    }
+
+    public void level(int i){
+        for (int j = i; j <=lui ; j++) {
+            System.out.print(">> "+ a[j]);
+        }
+    }
+
+}
---
+++ src/me/premaseem/datastructure/binaryTree/binaryTreeArray/BinaryTreeArrMain.java
@@ -0,0 +1,34 @@
+package me.premaseem.datastructure.binaryTree.binaryTreeArray;
+
+public class BinaryTreeArrMain {
+
+
+    public static void main(String[] args) {
+        BinaryTreeArr bsta = new BinaryTreeArr(15);
+
+        // insert
+        for (int i = 1; i <=15 ; i++) {
+            System.out.println("inserted " +bsta.insert(i));
+        }
+
+
+        // search
+        System.out.println("search result "+bsta.search(3));
+        System.out.println("search result "+bsta.search(17));
+
+        // delete
+//        System.out.println("delete result "+bsta.delete(7));
+        System.out.println("delete result "+bsta.delete(29));
+
+        // pre order traversal
+        bsta.pre(1);
+        System.out.println();
+        // In order traversal
+        bsta.in(1);
+        System.out.println();
+        // Post order traversal
+        bsta.post(1);
+        System.out.println();
+    }
+
+}
